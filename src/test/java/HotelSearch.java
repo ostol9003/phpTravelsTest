1d9b7f501512d6469a0896c8d45e177d949d3880
Ostolski Marcin <ostolski.marcin@gmail.com>
2023-02-17 21:54:08 +0100

    Hotels list + softAssert

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -1,12 +1,10 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
-import java.util.List;
 
 public class HotelSearch {
     @Test
