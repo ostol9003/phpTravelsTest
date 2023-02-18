e71ed1e993f5a9a92dfd4e3d1d2f39a91cc4f3cb
Ostolski Marcin <ostolski.marcin@gmail.com>
2023-02-18 14:04:30 +0100

    Deleted file 'HotelSearch_V1.java', method added to HotelSearch

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -12,6 +13,7 @@ import java.util.stream.Collectors;
 
 public class HotelSearch {
     @Test
+    /* Searching for hotel*/
     public void searchHotel() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
@@ -64,18 +66,78 @@ public class HotelSearch {
                 .collect(Collectors.toList());
 
 
-
         System.out.println(hotelsList.size());
         hotelsList.forEach(System.out::println);
 
         SoftAssert softAssert = new SoftAssert();
-        softAssert.assertEquals(hotelsList.get(0),"Jumeirah Beach Hotel","Failed to find");
-        softAssert.assertEquals(hotelsList.get(1),"Oasis Beach Tower","Failed to find");
-        softAssert.assertEquals(hotelsList.get(2),"Rose Rayhaan Rotana","Failed to find");
-        softAssert.assertEquals(hotelsList.get(3),"Hyatt Regency Perth","Failed to find");
+        softAssert.assertEquals(hotelsList.get(0), "Jumeirah Beach Hotel", "Failed to find");
+        softAssert.assertEquals(hotelsList.get(1), "Oasis Beach Tower", "Failed to find");
+        softAssert.assertEquals(hotelsList.get(2), "Rose Rayhaan Rotana", "Failed to find");
+        softAssert.assertEquals(hotelsList.get(3), "Hyatt Regency Perth", "Failed to find");
 
 
         driver.quit();
         softAssert.assertAll();
+    }
+
+    @Test
+    /* Searching for hotel without city name + assertion */
+    public void searchHotel_v1() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+
+
+        driver.findElement(By.name("checkin")).sendKeys("17/05/2023");
+        driver.findElement(By.name("checkin")).click();
+
+        driver.findElement(By.name("checkout")).click();
+        driver.findElements(By.xpath("//th[@class='switch' and text()='February 2023']"))
+                .stream()
+                .filter(WebElement::isDisplayed)
+                .findFirst()
+                .ifPresent(WebElement::click);
+
+        driver.findElements(By.xpath("//span[@class='month' and text()='May']"))
+                .stream()
+                .filter(WebElement::isDisplayed) //el -> el.isDisplayed()
+                .findFirst()
+                .ifPresent(WebElement::click); // el -> el.click()
+
+        driver.findElement(By.name("checkout")).click();
+
+        driver.findElements(By.xpath("//td[@class='day ' and text()='30']"))
+                .stream()
+                .filter(WebElement::isDisplayed)
+                .findFirst()
+                .ifPresent(WebElement::click);
+
+
+        driver.findElement(By.id("travellersInput")).click();
+        driver.findElement(By.id("childPlusBtn")).click();
+        driver.findElement(By.id("travellersInput")).click();
+
+        driver.findElement(By.xpath("//button[@type='submit' and text()=' Search']")).click();
+
+/*   // first option
+        List<String> hotelsList = driver.findElements(By.xpath("//h4[contains(@class,'list_title')]//b"))
+                .stream()
+                .map(el -> el.getAttribute("innerHTML"))
+                .toList();
+
+        Assert.assertTrue(hotelsList.size() > 0,"No results, hotels list empty!");
+        System.out.println(hotelsList.size());
+*/
+
+        // second option
+
+        WebElement noResultHeading = driver.findElement(By.xpath("//div[@class='itemscontainer']//h2"));
+        Assert.assertTrue(noResultHeading.isDisplayed());
+        Assert.assertEquals(noResultHeading.getText(), "No Results Found");
+        driver.quit();
+
     }
 }
